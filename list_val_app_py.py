f4be9532a6887feda78d08d9e12657f97a16d434
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-31 16:53:31 +0530

    Add files via upload

---
+++ list_val_app_py.py
@@ -0,0 +1,16 @@
+# 1.list3:store value , 2.print[2] value , 3.add value using append , 4.print all the value orderly
+# list using value in square brackets
+list3=[[1,2],[2,3],[4,5]]
+
+# find the value of n show single value using single value
+print(list3[2])
+
+# add another value using append
+list3.append([8,9])
+print(list3)
+
+#for loop using list of values in order
+for i in list3:
+    print(i)
+
+
